07f9e082e7cb44ed668b0df6165f5f58e66f810d
Ricardo Hernandez <richhquim@gmail.com>
2025-05-20 08:18:17 -0600

    practica1

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+import Header from './components/Header'
+import Parrafo from './components/Parrafo'
+import Tarjeta from './components/Tarjeta'
+
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className='flex justify-center ml-5'><Parrafo /></div>
+      <Tarjeta foto="/recursos/images/photo1.png" puesto="Product Owner" nombre="Bil Mahoney" />
+
+    </>
+
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button() {
+    return (
+        <div>Esto es un boton importado</div>
+    )
+}
+
+

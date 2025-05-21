e4e78f9758159bcb544095cb60674bda6a132895
Ricardo Hernandez <richhquim@gmail.com>
2025-05-21 11:15:26 -0600

    commit

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 
 
+// export default function Tarjeta(props) {
+//     return (
+//         <div className=' w-[180px] lg:w-[240px]  flex items-center justify-center flex-wrap relative'>
+//             <img className='w-[70%] lg:w-[100%]' src={props.foto} alt="foto" />
+//             <div className='w-[100%] h-[100%] absolute'>  <p className='w-[100%] text-black lg:text-2xl rotate-90 h-[100%] absolute right-[10%] -top-2'>{props.puesto}</p></div>
+//             <p className='w-[100%] text-black font-bold text-center lg:text-2xl'>{props.nombre}</p>
+//         </div>
+
+//     )
+// }
+
+
 export default function Tarjeta({ foto, puesto, nombre }) {
     return (
         <div className=' w-[180px] lg:w-[240px]  flex items-center justify-center flex-wrap relative'>
@@ -12,7 +24,4 @@ export default function Tarjeta({ foto, puesto, nombre }) {
 
     )
 }
-
-
-
 
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://ricardoahh.github.io/Proyecto1React/'
+  base: '/Proyecto1React'
 })
